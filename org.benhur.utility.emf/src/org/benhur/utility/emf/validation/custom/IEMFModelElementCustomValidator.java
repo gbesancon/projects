04c95f4ebbbddd3b01b56d792484041834a3deb8
Gilles Besançon <gilles.besancon@gmail.com>
2014-03-16 12:29:06 -0500

    Custom EMF Validation framework.

--- org.benhur.utility.emf/src/org/benhur/utility/emf/validation/custom/IEMFModelElementCustomValidator.java
+++ org.benhur.utility.emf/src/org/benhur/utility/emf/validation/custom/IEMFModelElementCustomValidator.java
@@ -6,5 +6,7 @@ import org.eclipse.emf.ecore.EObject;
 
 public interface IEMFModelElementCustomValidator<T extends EObject>
 {
+  Class<T> getModelElementClass();
+
   IStatus validate(T modelElement, IProgressMonitor progressMonitor);
 }
