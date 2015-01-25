8e250def08b652a61f109149526e002fc5e47820
Gilles Besancon <gilles.besancon@gmail.com>
2015-01-25 14:07:32 +0000

    Remove hiding mechanism

---
+++ app/src/main/java/org/benhur/bbingo/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package org.benhur.bbingo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.benhur.bbingo.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
