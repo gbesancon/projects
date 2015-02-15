f6131aac79b6b019f57b96492fb23844e0caea62
Gilles Besancon <gilles.besancon@gmail.com>
2015-02-15 20:27:38 +0000

    Implement coputation of shortest path but too long

--- CodinGame_20150206/src/Player.java
+++ CodinGame_20150206/src/Player.java
@@ -53,6 +53,42 @@ class Player
     }
   }
 
+  class GamePlayer
+  {
+    public int id = 0;
+    boolean isDead = false;
+    public Path path = new Path();
+    public int wallsLeft = 0;
+
+    @Override
+    public String toString()
+    {
+      return "id:" + id + ",path(" + path + "),wallsLeft:" + wallsLeft;
+    }
+  }
+
+  class Path
+  {
+    public List<DIRECTION> directions = new ArrayList<Player.DIRECTION>();
+    public List<Position> positions = new ArrayList<Player.Position>();
+
+    @Override
+    public String toString()
+    {
+      StringBuilder builder = new StringBuilder();
+      for (DIRECTION direction : directions)
+      {
+        builder.append(direction + ";");
+      }
+      builder.append(" => ");
+      for (Position position : positions)
+      {
+        builder.append(position + ";");
+      }
+      return builder.toString();
+    }
+  }
+
   class Position
   {
     public int x = 0;
@@ -73,7 +109,6 @@ class Player
 
   class Wall
   {
-    //
     public Position position = new Position(0, 0);
     public ORIENTATION orientation = ORIENTATION.VERTICAL;
 
@@ -82,6 +117,11 @@ class Player
     {
       return "position(" + position + "),orientation:" + orientation;
     }
+  }
+
+  enum ORIENTATION
+  {
+    HORIZONTAL, VERTICAL
   }
 
   abstract class Action
@@ -106,12 +146,12 @@ class Player
 
   enum DIRECTION
   {
-    NONE, UP, RIGHT, DOWN, LEFT
+    UP, RIGHT, DOWN, LEFT
   }
 
   class MoveAction extends Action
   {
-    public DIRECTION direction = DIRECTION.NONE;
+    public final DIRECTION direction;
 
     public MoveAction(DIRECTION direction)
     {
@@ -121,14 +161,15 @@ class Player
     @Override
     public int computeScore()
     {
-      int score = 0; // Case of DIRECTION.NONE
-      if (direction != DIRECTION.NONE)
+      int score = 0;
+      if (canMove(getMyLastPosition(), direction))
       {
-        if (canMove(GetMyPosition(), direction))
+        if ((getMyGamePlayer().path.directions.size() == 0)
+            || (getMyGamePlayer().path.directions.size() != 0 && direction != getOppositeDirection(getMyLastDirection())))
         {
           if (getMyPreferredDirection() == direction)
           {
-            score = 100;
+            score = 75;
           }
           else
           {
@@ -137,8 +178,13 @@ class Player
         }
         else
         {
-          score = -50;
+          // Can move but no progress, just backtracking
+          score = 25;
         }
+      }
+      else
+      {
+        score = -100;
       }
       return score;
     }
@@ -147,13 +193,8 @@ class Player
     public String toString()
     {
       // action: LEFT, RIGHT, UP, DOWN or "putX putY putOrientation" to place a wall
-      return direction != DIRECTION.NONE ? direction.toString() : "";
+      return direction.toString();
     }
-  }
-
-  enum ORIENTATION
-  {
-    HORIZONTAL, VERTICAL
   }
 
   class PuttWallAction extends Action
@@ -183,17 +224,112 @@ class Player
     }
   }
 
-  class GamePlayer
+  public GamePlayer getMyGamePlayer()
   {
-    public int id = 0;
-    public Position position = new Position(0, 0);
-    public int wallsLeft = 0;
+    return getGamePlayer(world.myId);
+  }
 
-    @Override
-    public String toString()
+  public GamePlayer getGamePlayer(int id)
+  {
+    return world.gamePlayers.get(id);
+  }
+
+  public Position getMyLastPosition()
+  {
+    return getLastPosition(world.myId);
+  }
+
+  public Position getLastPosition(int id)
+  {
+    return getGamePlayer(id).path.positions.get(0);
+  }
+
+  public DIRECTION getMyPreferredDirection()
+  {
+    return getPreferredDirection(world.myId);
+  }
+
+  public DIRECTION getPreferredDirection(int id)
+  {
+    final DIRECTION direction;
+    switch (id)
     {
-      return "id:" + id + ",position(" + position + "),wallsLeft:" + wallsLeft;
+      case 0:
+        direction = DIRECTION.RIGHT;
+        break;
+      case 1:
+        direction = DIRECTION.LEFT;
+        break;
+      case 2:
+        direction = DIRECTION.DOWN;
+        break;
+      default:
+        direction = null;
+        break;
     }
+    return direction;
+  }
+
+  public DIRECTION getMyLastDirection()
+  {
+    return getLastDirection(world.myId);
+  }
+
+  public DIRECTION getLastDirection(int id)
+  {
+    return getGamePlayer(id).path.directions.get(0);
+  }
+
+  public DIRECTION getDirection(Position position0, Position position1)
+  {
+    final DIRECTION direction;
+    int deltaX = position1.x - position0.x;
+    int deltaY = position1.y - position0.y;
+    if (deltaX > 0)
+    {
+      direction = DIRECTION.RIGHT;
+    }
+    else if (deltaX < 0)
+    {
+      direction = DIRECTION.LEFT;
+    }
+    else if (deltaY > 0)
+    {
+      direction = DIRECTION.DOWN;
+    }
+    else if (deltaY < 0)
+    {
+      direction = DIRECTION.UP;
+    }
+    else
+    {
+      direction = null;
+    }
+    return direction;
+  }
+
+  public DIRECTION getOppositeDirection(DIRECTION direction)
+  {
+    final DIRECTION oppositeDirection;
+    switch (direction)
+    {
+      case RIGHT:
+        oppositeDirection = DIRECTION.LEFT;
+        break;
+      case LEFT:
+        oppositeDirection = DIRECTION.RIGHT;
+        break;
+      case UP:
+        oppositeDirection = DIRECTION.DOWN;
+        break;
+      case DOWN:
+        oppositeDirection = DIRECTION.UP;
+        break;
+      default:
+        oppositeDirection = null;
+        break;
+    }
+    return oppositeDirection;
   }
 
   public static void main(String args[])
@@ -207,18 +343,43 @@ class Player
     int playerCount = in.nextInt(); // number of players (2,3, or 4)
     player.world.myId = in.nextInt(); // id of my player (0 = 1st player, 1 = 2nd player, ...)
 
+    for (int i = 0; i < playerCount; i++)
+    {
+      GamePlayer gamePlayer = player.new GamePlayer();
+      gamePlayer.id = i;
+      player.world.gamePlayers.add(gamePlayer);
+    }
+
     // game loop
     while (true)
     {
-      player.world.gamePlayers.clear();
       for (int i = 0; i < playerCount; i++)
       {
-        GamePlayer gamePlayer = player.new GamePlayer();
-        gamePlayer.id = i;
-        gamePlayer.position.x = in.nextInt(); // x-coordinate of the player
-        gamePlayer.position.y = in.nextInt(); // y-coordinate of the player
-        gamePlayer.wallsLeft = in.nextInt(); // number of walls available for the player
-        player.world.gamePlayers.add(gamePlayer);
+        GamePlayer gamePlayer = player.getGamePlayer(i);
+        int x = in.nextInt(); // x-coordinate of the player
+        int y = in.nextInt(); // y-coordinate of the player
+        int wallsLeft = in.nextInt(); // number of walls available for the player
+        if (!gamePlayer.isDead)
+        {
+          if (x == -1 && y == -1 && wallsLeft == -1)
+          {
+            gamePlayer.isDead = true;
+          }
+          else
+          {
+            if (gamePlayer.path.positions.size() == 0)
+            {
+              gamePlayer.path.positions.add(0, player.new Position(x, y));
+            }
+            else
+            {
+              gamePlayer.path.positions.add(0, player.new Position(x, y));
+              gamePlayer.path.directions.add(0, player.getDirection(gamePlayer.path.positions.get(1),
+                                                                    gamePlayer.path.positions.get(0)));
+            }
+            gamePlayer.wallsLeft = wallsLeft;
+          }
+        }
       }
 
       int wallCount = in.nextInt(); // number of walls on the board
@@ -241,124 +402,108 @@ class Player
     }
   }
 
-  public DIRECTION getMyPreferredDirection()
+  public String computeAction()
   {
-    DIRECTION direction = DIRECTION.NONE;
-    switch (world.myId)
+    List<Action> actions = computePotentialActions();
+    int tie = sortActionsByScore(actions);
+    System.err.println("tie:" + tie);
+    for (Action action : actions)
     {
-      case 0:
-        direction = DIRECTION.RIGHT;
-        break;
-      case 1:
-        direction = DIRECTION.LEFT;
-        break;
-      case 2:
-        direction = DIRECTION.DOWN;
-        break;
-      case 3:
-        direction = DIRECTION.UP;
-        break;
-      default:
-        break;
+      System.err.println(action + ":" + action.getScore());
     }
-    return direction;
-  }
-
-  public Position computeNextPosition(Position position, DIRECTION direction)
-  {
-    Position nextPosition = null;
-    switch (direction)
-    {
-      case NONE:
-        nextPosition = position;
-        break;
-      case UP:
-        nextPosition = new Position(position.x, position.y - 1);
-        break;
-      case RIGHT:
-        nextPosition = new Position(position.x + 1, position.y);
-        break;
-      case DOWN:
-        nextPosition = new Position(position.x, position.y + 1);
-        break;
-      case LEFT:
-        nextPosition = new Position(position.x - 1, position.y);
-        break;
-      default:
-        break;
-    }
-    return nextPosition;
-  }
-
-  public boolean isRunningIntoWall(Position position, DIRECTION direction)
-  {
-    boolean isRunningIntoWall = false;
-    for (Wall wall : world.walls)
-    {
-      if (wall.orientation == ORIENTATION.HORIZONTAL)
-      {
-        boolean validateX = position.x >= wall.position.x && position.x < wall.position.x + 2;
-        if (direction == DIRECTION.UP)
-        {
-          isRunningIntoWall |= validateX && (position.y == wall.position.y);
-        }
-        else if (direction == DIRECTION.DOWN)
-        {
-          isRunningIntoWall |= validateX && (position.y + 1 == wall.position.y);
-        }
-        else
-        {
-          // Going Left or Right : no problem
-        }
-      }
-      else
-      {
-        boolean validateY = position.y >= wall.position.y && position.y < wall.position.y + 2;
-        if (direction == DIRECTION.LEFT)
-        {
-          isRunningIntoWall |= (position.x == wall.position.x) && validateY;
-        }
-        else if (direction == DIRECTION.RIGHT)
-        {
-          isRunningIntoWall |= (position.x + 1 == wall.position.x) && validateY;
-        }
-        else
-        {
-          // Going Up or /Down : no problem
-        }
-      }
-    }
-    return isRunningIntoWall;
-  }
-
-  public boolean canMove(Position position, DIRECTION direction)
-  {
-    boolean canMove = false;
-    Position nextPosition = computeNextPosition(position, direction);
-    if (world.board.isInside(nextPosition))
-    {
-      if (!isRunningIntoWall(position, direction))
-      {
-        canMove = true;
-      }
-    }
-    return canMove;
-  }
-
-  public Position GetMyPosition()
-  {
-    return world.gamePlayers.get(world.myId).position;
+    return selectAction(actions, tie).toString();
   }
 
   public List<Action> computePotentialActions()
   {
     List<Action> actions = new ArrayList<Player.Action>();
-    actions.add(new MoveAction(DIRECTION.NONE));
-    actions.add(new MoveAction(DIRECTION.UP));
-    actions.add(new MoveAction(DIRECTION.RIGHT));
-    actions.add(new MoveAction(DIRECTION.DOWN));
-    actions.add(new MoveAction(DIRECTION.LEFT));
+    int algorithm = 0;
+    if (algorithm == 0)
+    {
+      actions.add(new MoveAction(DIRECTION.RIGHT));
+      actions.add(new MoveAction(DIRECTION.LEFT));
+      actions.add(new MoveAction(DIRECTION.UP));
+      actions.add(new MoveAction(DIRECTION.DOWN));
+    }
+    else if (algorithm == 1)
+    {
+      List<DIRECTION> shortestPathGuidance = computeShortestPathGuidance();
+      actions.add(new MoveAction(shortestPathGuidance.get(0)));
+    }
     return actions;
+  }
+
+  public List<DIRECTION> computeShortestPathGuidance()
+  {
+    List<DIRECTION> guidance = new ArrayList<Player.DIRECTION>();
+    computeShortestPathGuidance(getMyLastPosition(), getMyPreferredDirection(), guidance);
+    return guidance;
+  }
+
+  public boolean computeShortestPathGuidance(Position position, DIRECTION preferredDirection, List<DIRECTION> guidance)
+  {
+    boolean foundPath = false;
+    // Stop conditions
+    if ((preferredDirection == DIRECTION.RIGHT && position.x == world.board.width - 1)
+        || (preferredDirection == DIRECTION.LEFT && position.x == 0)
+        || (preferredDirection == DIRECTION.UP && position.y == 0)
+        || (preferredDirection == DIRECTION.DOWN && position.y == world.board.height - 1))
+    {
+      guidance.add(preferredDirection);
+      foundPath = true;
+    }
+    else if (guidance.size() > 14)
+    {
+      // Cut infinite loop
+      foundPath = false;
+    }
+    // Search the path
+    else
+    {
+      DIRECTION[] directions = new DIRECTION[4];
+      int iDirection = 0;
+      directions[iDirection++] = preferredDirection;
+      for (DIRECTION direction : DIRECTION.values())
+      {
+        if (direction != preferredDirection)
+        {
+          directions[iDirection++] = direction;
+        }
+      }
+      DIRECTION shortestPathNextDirection = null;
+      List<DIRECTION> shortestPathTmpGuidance = null;
+      for (DIRECTION direction : directions)
+      {
+        if (canMove(getMyLastPosition(), direction))
+        {
+          List<DIRECTION> tmpGuidance = new ArrayList<Player.DIRECTION>();
+          tmpGuidance.addAll(guidance);
+          tmpGuidance.add(direction);
+          Position nextPosition = computeNextPosition(position, direction);
+          boolean foundSubPath = computeShortestPathGuidance(nextPosition, preferredDirection, tmpGuidance);
+          if (foundSubPath)
+          {
+            if (shortestPathTmpGuidance == null || (tmpGuidance.size() < shortestPathTmpGuidance.size()))
+            {
+              shortestPathNextDirection = direction;
+              shortestPathTmpGuidance = tmpGuidance;
+            }
+          }
+        }
+      }
+
+      if (shortestPathNextDirection != null)
+      {
+        guidance.clear();
+        guidance.addAll(shortestPathTmpGuidance);
+        foundPath = true;
+      }
+      else
+      {
+        foundPath = false;
+      }
+    }
+    return foundPath;
   }
 
   public int sortActionsByScore(List<Action> actions)
@@ -397,15 +542,89 @@ class Player
     return actions.get(iIndex);
   }
 
-  public String computeAction()
+  public boolean canMove(Position position, DIRECTION direction)
   {
-    List<Action> actions = computePotentialActions();
-    int tie = sortActionsByScore(actions);
-    System.err.println("tie:" + tie);
-    for (Action action : actions)
+    boolean canMove = false;
+    Position nextPosition = computeNextPosition(position, direction);
+    if (world.board.isInside(nextPosition))
     {
-      System.err.println(action + ":" + action.getScore());
+      if (!isRunningIntoWall(position, direction))
+      {
+        canMove = true;
+      }
     }
-    return selectAction(actions, tie).toString();
+    return canMove;
+  }
+
+  public Position computeNextPosition(Position position, DIRECTION direction)
+  {
+    final Position nextPosition;
+    switch (direction)
+    {
+      case UP:
+        nextPosition = new Position(position.x, position.y - 1);
+        break;
+      case RIGHT:
+        nextPosition = new Position(position.x + 1, position.y);
+        break;
+      case DOWN:
+        nextPosition = new Position(position.x, position.y + 1);
+        break;
+      case LEFT:
+        nextPosition = new Position(position.x - 1, position.y);
+        break;
+      default:
+        nextPosition = null;
+        break;
+    }
+    return nextPosition;
+  }
+
+  public boolean isRunningIntoWall(Position position, DIRECTION direction)
+  {
+    boolean isRunningIntoWall = false;
+    for (Wall wall : world.walls)
+    {
+      isRunningIntoWall |= isRunningIntoWall(position, direction, wall);
+    }
+    return isRunningIntoWall;
+  }
+
+  public boolean isRunningIntoWall(Position position, DIRECTION direction, Wall wall)
+  {
+    boolean isRunningIntoWall = false;
+    if (wall.orientation == ORIENTATION.HORIZONTAL)
+    {
+      boolean validateX = position.x >= wall.position.x && position.x < wall.position.x + 2;
+      if (direction == DIRECTION.UP)
+      {
+        isRunningIntoWall = validateX && (position.y == wall.position.y);
+      }
+      else if (direction == DIRECTION.DOWN)
+      {
+        isRunningIntoWall = validateX && (position.y + 1 == wall.position.y);
+      }
+      else
+      {
+        // Going Left or Right : no problem
+      }
+    }
+    else
+    {
+      boolean validateY = position.y >= wall.position.y && position.y < wall.position.y + 2;
+      if (direction == DIRECTION.LEFT)
+      {
+        isRunningIntoWall = (position.x == wall.position.x) && validateY;
+      }
+      else if (direction == DIRECTION.RIGHT)
+      {
+        isRunningIntoWall = (position.x + 1 == wall.position.x) && validateY;
+      }
+      else
+      {
+        // Going Up or Down : no problem
+      }
+    }
+    return isRunningIntoWall;
   }
 }
