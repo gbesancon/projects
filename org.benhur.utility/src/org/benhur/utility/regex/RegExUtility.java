e669d8dc0537ed616da8a6d05954f80223743404
Gilles Besancon <gilles.besancon@gmail.com>
2015-05-04 11:31:08 +0100

    Add a check value method to the RegExUtility class.

--- org.benhur.utility/src/org/benhur/utility/regex/RegExUtility.java
+++ org.benhur.utility/src/org/benhur/utility/regex/RegExUtility.java
@@ -2,6 +2,8 @@ package org.benhur.utility.regex;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegExUtility
 {
@@ -38,5 +40,31 @@ public class RegExUtility
       }
     }
     return stringBuilder.toString();
+  }
+
+  public static boolean checkValue(String value, String valueIncludePatternString, String valueExcludePatternString)
+  {
+    boolean acceptValue = false;
+    boolean valueInclude = true; // By default accept all project names
+    if (!valueIncludePatternString.equalsIgnoreCase(""))
+    {
+      Pattern valueIncludePattern = Pattern.compile(valueIncludePatternString);
+      Matcher valueIncludeMatcher = valueIncludePattern.matcher(value);
+      valueInclude = valueIncludeMatcher.find();
+    }
+
+    boolean valueExclude = false; // By default we exclude none of the project names
+    if (!valueExcludePatternString.equalsIgnoreCase(""))
+    {
+      Pattern valueExcludePattern = Pattern.compile(valueExcludePatternString);
+      Matcher valueExcludeMatcher = valueExcludePattern.matcher(value);
+      valueExclude = valueExcludeMatcher.find();
+    }
+
+    if (valueInclude && !valueExclude)
+    {
+      acceptValue = true;
+    }
+    return acceptValue;
   }
 }
