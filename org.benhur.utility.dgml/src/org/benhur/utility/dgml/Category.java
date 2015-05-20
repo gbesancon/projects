a24a5153efae87e8c728c2cefa902f479bd0f5e6
Gilles Besancon <gilles.besancon@gmail.com>
2015-05-20 09:29:08 +0100

    Create utility project to generate DGML files.

---
+++ org.benhur.utility.dgml/src/org/benhur/utility/dgml/Category.java
@@ -0,0 +1,27 @@
+package org.benhur.utility.dgml;
+
+public class Category
+{
+  protected final String id;
+  protected final String label;
+  protected final String description;
+  protected final boolean canBeDataDriven;
+  protected final boolean canLinkedNodesBeDataDriven;
+  protected final String incomingActionLabel;
+  protected final boolean isContainment;
+  protected final String outgoingActionLabel;
+
+  public Category(String id, String label, String description, boolean canBeDataDriven,
+                  boolean canLinkedNodesBeDataDriven, String incomingActionLabel, boolean isContainment,
+                  String outgoingActionLabel)
+  {
+    this.id = id;
+    this.label = label;
+    this.description = description;
+    this.canBeDataDriven = canBeDataDriven;
+    this.canLinkedNodesBeDataDriven = canLinkedNodesBeDataDriven;
+    this.incomingActionLabel = incomingActionLabel;
+    this.isContainment = isContainment;
+    this.outgoingActionLabel = outgoingActionLabel;
+  }
+}
