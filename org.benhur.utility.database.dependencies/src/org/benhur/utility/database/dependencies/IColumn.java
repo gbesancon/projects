f4db3cbfa3f4f2838e165f84fcbbdddd87684723
GBesancon <gilles.besancon@gmail.com>
2015-05-24 19:24:00 +0100

    Create DSM File Builder for Database

--- org.benhur.utility.database.dependencies/src/org/benhur/utility/database/dependencies/IColumn.java
+++ org.benhur.utility.database.dependencies/src/org/benhur/utility/database/dependencies/IColumn.java
@@ -2,6 +2,8 @@ package org.benhur.utility.database.dependencies;
 
 public interface IColumn
 {
+  ITable getTable();
+
   String getId();
 
   String getName();
--- org.benhur.utility.database.dependencies/src/org/benhur/utility/database/dependencies/Table.java
+++ org.benhur.utility.database.dependencies/src/org/benhur/utility/database/dependencies/Table.java
@@ -5,15 +5,23 @@ import java.util.List;
 
 public class Table implements ITable
 {
+  protected final IDatabase database;
   protected final String id;
   protected final String name;
   protected final List<IColumn> columns;
 
-  public Table(String id, String name)
+  public Table(IDatabase database, String id, String name)
   {
+    this.database = database;
     this.id = id;
     this.name = name;
     this.columns = new ArrayList<>();
+  }
+
+  @Override
+  public IDatabase getDatabasse()
+  {
+    return database;
   }
 
   @Override
