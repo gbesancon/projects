f26a7af48f714488acc1551638656c00db4c6567
Gilles Besancon <gilles.besancon@gmail.com>
2015-05-28 18:15:41 +0100

    Rename configuration file and update default start.bat file

---
+++ org.benhur.utility.database.dependencies.dgml/src/org/benhur/utility/database/dependencies/dgml/MainDatabaseDependenciesDGML.java
@@ -0,0 +1,42 @@
+package org.benhur.utility.database.dependencies.dgml;
+
+import java.io.IOException;
+
+import javax.xml.bind.PropertyException;
+
+import org.benhur.utility.database.dependencies.IDatabase;
+import org.benhur.utility.database.dependencies.builder.DatabaseBuilder;
+import org.benhur.utility.database.dependencies.configuration.Configuration;
+
+public class MainDatabaseDependenciesDGML
+{
+  public static void main(String[] args)
+  {
+    if (args.length == 1)
+    {
+      try
+      {
+        Configuration configuration = new Configuration(args[0]);
+        DatabaseBuilder databaseBuilder = new DatabaseBuilder();
+        IDatabase database = databaseBuilder.buildDatabase(configuration.getHost(), configuration.getPort(),
+                                                           configuration.getUsername(), configuration.getPassword(),
+                                                           configuration.getDatabaseName());
+
+        DatabaseDGMLFileBuilder databaseDGMLFileBuilder = new DatabaseDGMLFileBuilder();
+        databaseDGMLFileBuilder.createDGMLFile(database, configuration, database.getName() + ".dgml");
+      }
+      catch (PropertyException e)
+      {
+        e.printStackTrace();
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+    }
+    else
+    {
+      System.err.println("No configuration file specified");
+    }
+  }
+}
