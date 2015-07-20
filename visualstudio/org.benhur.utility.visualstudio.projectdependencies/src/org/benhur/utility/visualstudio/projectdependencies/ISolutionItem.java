9e70b4c392fea8905bfb055ae56a0b2c8c318ee2
Gilles Besançon <gilles.besancon@gmail.com>
2015-07-20 23:05:37 +0100

    Add include-what-you-use and metrix++ and visualstudio.projectdependencies

---
+++ visualstudio/org.benhur.utility.visualstudio.projectdependencies/src/org/benhur/utility/visualstudio/projectdependencies/ISolutionItem.java
@@ -0,0 +1,16 @@
+package org.benhur.utility.visualstudio.projectdependencies;
+
+import java.util.List;
+
+public interface ISolutionItem
+{
+  ISolution getSolution();
+
+  String getId();
+
+  String getName();
+
+  void addProjectDependency(IProjectDependency projectDependency);
+
+  List<IProjectDependency> getProjectDependencies();
+}
---
+++ visualstudio/org.benhur.utility.visualstudio.projectdependencies/src/org/benhur/utility/visualstudio/projectdependencies/group/Group.java
@@ -0,0 +1,58 @@
+package org.benhur.utility.visualstudio.projectdependencies.group;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.benhur.utility.regex.RegExUtility;
+import org.benhur.utility.visualstudio.projectdependencies.ISolutionItem;
+
+public class Group
+{
+  protected final String name;
+  protected final String includePatternString;
+  protected final String excludePatternString;
+  protected final List<ISolutionItem> solutionItems = new ArrayList<>();
+  protected final List<Group> subGroups = new ArrayList<>();
+
+  public Group(String name, String includePatternString, String excludePatternString)
+  {
+    this.name = name;
+    this.includePatternString = includePatternString;
+    this.excludePatternString = excludePatternString;
+  }
+
+  public String getId()
+  {
+    return name;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public boolean acceptSolutionItem(ISolutionItem solutionItem)
+  {
+    return RegExUtility.checkValue(solutionItem.getName(), includePatternString, excludePatternString);
+  }
+
+  public void addSolutionItem(ISolutionItem solutionItem)
+  {
+    solutionItems.add(solutionItem);
+  }
+
+  public List<ISolutionItem> getProjects()
+  {
+    return solutionItems;
+  }
+
+  public void addSubGroup(Group subGroup)
+  {
+    subGroups.add(subGroup);
+  }
+
+  public List<Group> getSubGroups()
+  {
+    return subGroups;
+  }
+}
