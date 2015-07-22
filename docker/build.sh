e46a79a9956c3bf9ac1f41c6297558f1cc459845
Gilles Besancon <gilles.besancon@gmail.com>
2015-07-22 17:35:11 +0100

    groups scripts in a scripts folder

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,1 @@
-mkdir content
-cp ../fortune-cowsay-lolcat-server.sh content/
-cp ../ansi2html.sh content/
-docker build -t fortune-cowsay-lolcat-server .
-rm -rf content/
+docker build -t fortune-cowsay-lolcat-server -f ./Dockerfile ..
