5ef692d70f24aac8248f693d463cbdab9ea51cd2
Gilles Besançon <gilles.besancon@gmail.com>
2016-02-07 09:51:46 +0000

    Add Unit-Test for all classes Reach 95% coverage with basic data (not representative, would need better test data)

--- org.benhur.jpmorgan.supersimplestocks/test/org/benhur/jpmorgan/supersimplestocks/StockFormulasTest.java
+++ org.benhur.jpmorgan.supersimplestocks/test/org/benhur/jpmorgan/supersimplestocks/StockFormulasTest.java
@@ -2,7 +2,9 @@ package org.benhur.jpmorgan.supersimplestocks;
 
 import static org.junit.Assert.assertEquals;
 
-import org.benhur.jpmorgan.supersimplestocks.StockFormulas;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.benhur.jpmorgan.supersimplestocks.data.Stock;
 import org.benhur.jpmorgan.supersimplestocks.data.Stock.Type;
 import org.benhur.jpmorgan.supersimplestocks.data.Trade;
@@ -25,39 +27,49 @@ public class StockFormulasTest
   @Test
   public void testComputeDividendYieldFromLastDividend()
   {
-    Stock stock1 = new Stock("GIN", Type.COMMON, 8.0, 0.02, 100.0);
-    assertEquals(0.8, StockFormulas.ComputeDividendYield(stock1, 10.0), DELTA);
-    Stock stock2 = new Stock("GIN", Type.PREFERRED, 8.0, 0.02, 100.0);
-    assertEquals(0.2, StockFormulas.ComputeDividendYield(stock2, 10.0), DELTA);
+    Stock stock1 = new Stock("GIN", Type.COMMON, 0.08, 0.02, 1);
+    assertEquals(0.8, StockFormulas.ComputeDividendYield(stock1, 0.1), DELTA);
+    Stock stock2 = new Stock("GIN", Type.PREFERRED, 0.08, 0.02, 1);
+    assertEquals(0.2, StockFormulas.ComputeDividendYield(stock2, 0.1), DELTA);
   }
 
   @Test
   public void testComputePERatio()
   {
-    Stock stock1 = new Stock("GIN", Type.COMMON, 8.0, 0.02, 100.0);
-    assertEquals(1.25, StockFormulas.ComputePERatio(stock1, 10.0), DELTA);
-    Stock stock2 = new Stock("GIN", Type.PREFERRED, 8.0, 0.02, 100.0);
-    assertEquals(5.0, StockFormulas.ComputePERatio(stock2, 10.0), DELTA);
+    Stock stock1 = new Stock("GIN", Type.COMMON, 0.08, 0.02, 1);
+    assertEquals(1.25, StockFormulas.ComputePERatio(stock1, 0.1), DELTA);
+    Stock stock2 = new Stock("GIN", Type.PREFERRED, 0.08, 0.02, 1);
+    assertEquals(5.0, StockFormulas.ComputePERatio(stock2, 0.1), DELTA);
   }
 
   @Test
   public void testComputeGeometricMean()
   {
-    double[] prices = { 10.0, 9.0, 8.0, 9.0, 10.0, 11.0, 12.0 };
-    assertEquals(9.779, StockFormulas.ComputeGeometricMean(prices), DELTA);
+    Stock stock = new Stock("TEA", org.benhur.jpmorgan.supersimplestocks.data.Stock.Type.COMMON, 0.0, 0.0, 1.0);
+    List<Trade> trades = new ArrayList<>();
+    trades.add(new Trade(stock, Indicator.BUY, 0.10, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.09, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.08, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.09, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.1, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.11, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.12, 10, System.currentTimeMillis()));
+    assertEquals(0.097, StockFormulas.ComputeGeometricMean(trades), DELTA);
   }
 
   @Test
   public void testComputeVolumeWeightedStockPrice()
   {
-    Stock stock = new Stock("TEA", org.benhur.jpmorgan.supersimplestocks.data.Stock.Type.COMMON, 0.0, 0.0, 100.0);
-    Trade[] trade = { new Trade(stock, Indicator.BUY, 10.0, 10.0, System.currentTimeMillis()),
-        new Trade(stock, Indicator.BUY, 9.0, 10.0, System.currentTimeMillis()),
-        new Trade(stock, Indicator.BUY, 8.0, 10.0, System.currentTimeMillis()),
-        new Trade(stock, Indicator.BUY, 9.0, 10.0, System.currentTimeMillis()),
-        new Trade(stock, Indicator.BUY, 10.0, 10.0, System.currentTimeMillis()),
-        new Trade(stock, Indicator.BUY, 11.0, 10.0, System.currentTimeMillis()),
-        new Trade(stock, Indicator.BUY, 12.0, 10.0, System.currentTimeMillis()) };
-    assertEquals(9.857, StockFormulas.ComputeVolumeWeightedStockPrice(trade), DELTA);
+    Stock stock = new Stock("TEA", org.benhur.jpmorgan.supersimplestocks.data.Stock.Type.COMMON, 0.0, 0.0, 1.0);
+    List<Trade> trades = new ArrayList<>();
+    trades.add(new Trade(stock, Indicator.BUY, 0.10, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.09, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.08, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.09, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.1, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.11, 10, System.currentTimeMillis()));
+    trades.add(new Trade(stock, Indicator.BUY, 0.12, 10, System.currentTimeMillis()));
+
+    assertEquals(0.0985, StockFormulas.ComputeVolumeWeightedStockPrice(trades), DELTA);
   }
 }
