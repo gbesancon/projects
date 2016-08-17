f4d4d650c8fce13044bff2886eddc544b7dd5154
Gilles Besancon <gilles.besancon@gmail.com>
2016-08-17 23:15:45 +0100

    Refactor make.sh and output PDF

--- make.sh
+++ make.sh
@@ -1,7 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+check_tools ()
+{
+  command -v echo > /dev/null 2>&1 || { echo >&2 "echo is not installed.  Aborting."; exit 1; }
+  command -v pdflatex > /dev/null 2>&1 || { echo >&2 "pdflatex is not installed.  Aborting."; exit 1; }
+}
+
+generate_pdf ()
+{
+  echo Generate PDF for $1
+  pdflatex -synctex=1 -interaction=nonstopmode $1 > $1.log
+}
+
+REFERENCE_FOLDER=`pwd`
+check_tools
 
 for FILE in resume_en.tex resume_fr.tex
 do 
-  pdflatex -synctex=1 -interaction=nonstopmode $FILE 
+  generate_pdf $FILE 
 done
 
