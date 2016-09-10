b76113d069975e570dccc8bf95b1fcaf5896d89d
Gilles Besancon <gilles.besancon@gmail.com>
2016-09-10 19:24:34 +0100

    Refactor mplayer and vlc wrapper scripts and create make.sh/setup.sh scripts

--- youtube/mplayer/youtube-mplayer-caca.sh
+++ youtube/mplayer/youtube-mplayer-caca.sh
@@ -1,1 +1,13 @@
-../youtube-dl $1 -o - | mplayer -vo caca -
+#!/bin/bash
+
+check_usage ()
+{
+  if [ $# -neq 1 ] ; then
+    echo "Usage: $0 YOUTUBE_URL"
+    exit 1
+  fi
+}
+
+check_usage "$@"
+./youtube-mplayer.sh $1 caca
+
