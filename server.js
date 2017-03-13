81294b89dfada3b974cca3c30b12f5d9d848ac55
Gilles Besancon <gilles.besancon@gmail.com>
2017-03-13 22:55:25 +0000

    setup mode js server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var Sound = require('node-aplay');
+var request = require('request');
+var myIP = require('my-ip');
+
+app.get('/play', function (req, res) {	 
+	// fire and forget: 
+	new Sound('/usr/src/app/starwars.wav').play();
+	res.send('Music!!!');
+});
+
+//start a server on port 80 and log its start to our console
+var server = app.listen(80, function () {
+  var port = server.address().port;
+});
