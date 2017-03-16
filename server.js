890eb05b52a6cd1828d93ab309484cad16dbf838
Gilles Besançon <gilles.besancon@gmail.com>
2017-03-16 21:54:57 +0000

    Remove git repository but keep the library

--- server.js
+++ server.js
@@ -7,12 +7,24 @@ app.get('/', function (req, res) {
 	res.send('');
 });
 
-app.get('/play', function (req, res) {	 
+app.get('/music', function (req, res) {	 
 	new sound('/usr/src/app/speaker-phat/test/test.wav').play();
 	res.send('Music !!!');
 });
 
-//start a server on port 80 and log its start to our console
+app.get('/led', function (req, res) {	 
+	exec('python /usr/src/app/led.py',
+		function (error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+		});
+	res.send('LED !!!');
+});
+
+// start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
   var port = server.address().port;
 });
