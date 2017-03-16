5e7f41a06964e932e58faf19c0c1be720061730b
Gilles Besancon <gilles.besancon@gmail.com>
2017-03-16 21:24:30 +0000

    Add index page:x

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
-var Sound = require('node-aplay');
+var sound = require('node-aplay');
 var request = require('request');
-var myIP = require('my-ip');
+
+app.get('/', function (req, res) {	 
+	res.send('');
+});
 
 app.get('/play', function (req, res) {	 
-	// fire and forget: 
-	new Sound('/usr/src/app/speaker-phat/test/test.wav').play();
-	res.send('Music!!!');
+	new sound('/usr/src/app/speaker-phat/test/test.wav').play();
+	res.send('Music !!!');
 });
 
 //start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
   var port = server.address().port;
 });
+
