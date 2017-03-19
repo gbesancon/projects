879508c998c66d9eaada716d30c9dbd9905e7084
Gilles Besancon <gilles.besancon@gmail.com>
2017-03-19 17:56:24 +0000

    Update unit time

--- led.py
+++ led.py
@@ -7,32 +7,40 @@ import sys
 import morse
 import speakerphat
 
+TIME_FOR_UNIT = 0.1
+DOT = '.'
+DASH = '-'
+SPACE = ' '
+
 def displayText( text ):
 	morseCode = morse.convertTextToMorseCode(text)
 	displayMorseCode( morseCode )
 
 def displayMorseCode( morseCode ):
 	for char in morseCode:
-		if char == '.':
+		if char == DOT:
 			displayDot()
-		elif char == '-':
+		elif char == DASH:
 			displayDash()
-		else:
+		elif char == SPACE:
 			displaySpace()
-		time.sleep(1)
+	displaySpace()
 
 def displayDot():
 	speakerphat.clear()
 	speakerphat.set_leds([0,0,0,0,255,255,0,0,0,0])
 	speakerphat.show()
+	time.sleep(TIME_FOR_UNIT)
 
 def displayDash():
 	speakerphat.clear()
 	speakerphat.set_leds([255,255,255,255,255,255,255,255,255,255])
 	speakerphat.show()
+	time.sleep(3*TIME_FOR_UNIT)
 
 def displaySpace():
 	speakerphat.clear()
+	time.sleep(7*TIME_FOR_UNIT)
 
 def main():
 	text = sys.argv[1]
