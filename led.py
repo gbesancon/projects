e879674d315759a98d37d4e794176ca511c14f0a
Gilles Besancon <gilles.besancon@gmail.com>
2017-03-19 17:25:54 +0000

    Add text -> Morse converter

--- led.py
+++ led.py
@@ -4,11 +4,40 @@ import math
 import time
 import sys
 
+import morse
 import speakerphat
 
-for i in range(1000):
-    offset = 0
-    speakerphat.clear()
-    speakerphat.set_led(offset,255)
-    speakerphat.show()
+def displayText( text ):
+	morseCode = convertTextToMorseCode(text)
+	displayMorseCode( morseCode )
+
+def displayMorseCode( morseCode ):
+	for char in morseCode:
+		if char == '.':
+			displayDot()
+		elif char == '-':
+			displayDash()
+		else:
+			displaySpace()
+		time.sleep(5)
+
+def displayDot():
+	speakerphat.clear()
+	speakerphat.set_led([0,0,0,0,255,255,0,0,0,0])
+	speakerphat.show()
+
+def displayDash():
+	speakerphat.clear()
+	speakerphat.set_led([255,255,255,255,255,255,255,255,255,255])
+	speakerphat.show()
+
+def displaySpace():
+	speakerphat.clear()
+
+def main():
+	text = sys.argv[1]
+	displayText( text )
+
+if __name__ == "__main__":
+	main()
 
--- speakerphat.py
+++ speakerphat.py
@@ -1,3 +1,4 @@
+# Updated from: 
 # git clone https://github.com/pimoroni/speaker-phat.git
 # speaker-phat/python/speakerphat.py
 
@@ -24,15 +25,19 @@ for x in stupid_led_mappings:
 sn3218.enable_leds(enable_leds)
 sn3218.enable()
 
-def set_led(index, value):
-    led_values[stupid_led_mappings[index]] = value
-
-def show():
-    sn3218.output(led_values)
-
 def clear():
     global led_values
     led_values = [0 for x in range(18)]
     show()
+
+def set_led(index, value):
+    led_values[stupid_led_mappings[index]] = value
+
+def set_leds(values):
+    for index in range(len(values)):
+    	set_led(index, values[index]) 
+
+def show():
+    sn3218.output(led_values)
 
 atexit.register(clear)
