bba9ea9763e372121c41cfa493479533e298105d
Gilles Besançon <gilles.besancon@gmail.com>
2017-03-19 21:44:54 +0000

    use pygame to play sound

--- playMorse.py
+++ playMorse.py
@@ -4,15 +4,16 @@ import math
 import time
 import sys
 
+import pygame
+
 import morse
-import speakerphat-audio
 import speakerphat-led
 
-def playText( text ):
+def outputText(text):
 	morseCode = morse.convertTextToMorseCode(text)
-	playMorseCode( morseCode )
+	outputMorseCode( morseCode )
 
-def playMorseCode( morseCode ):
+def outputMorseCode(morseCode):
 	for char in morseCode:
 		if   char == morse.DOT:
 			playDot()
@@ -28,25 +29,43 @@ def playMorseCode( morseCode ):
 			time.sleep(morse.TIME_FOR_WORD_SEPARATOR)
 	speakerphat-led.clear()
 
-def playDot():
+def outputMorseCodeCharacter(character):
+	if   char == morse.DOT:
+		outputDot()
+		time.sleep(morse.TIME_FOR_DOT)
+	elif char == morse.DASH:
+		outputDash()
+		time.sleep(morse.TIME_FOR_DASH)
+	elif char == morse.LETTER_SEPARATOR:
+		outputLetterSeparator()
+		time.sleep(morse.TIME_FOR_LETTER_SEPARATOR)
+	elif char == morse.WORD_SEPARATOR:
+		outputWordSeparator()
+		time.sleep(morse.TIME_FOR_WORD_SEPARATOR)
+
+def outputDot():
 	speakerphat-led.clear()
 	speakerphat-led.set_leds([0,0,0,0,255,255,0,0,0,0])
 	speakerphat-led.show()
+	pygame.mixer.music.load('dot.wav')
+	pygame.mixer.music.play()
 
-def playDash():
+def outputDash():
 	speakerphat-led.clear()
 	speakerphat-led.set_leds([255,255,255,255,255,255,255,255,255,255])
 	speakerphat-led.show()
+	pygame.mixer.music.load('dash.wav')
+	pygame.mixer.music.play()
 
-def playLetterSeparator():
+def outputLetterSeparator():
 	speakerphat-led.clear()
 
-def playWordSeparator():
+def outputWordSeparator():
 	speakerphat-led.clear()
 
 def main():
 	text = sys.argv[1]
-	playText( text )
+	outputText(text)
 
 if __name__ == "__main__":
 	main()
