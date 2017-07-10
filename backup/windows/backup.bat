3d6cfcc108fec7f904bb6d590c83a37fcf31195d
Gilles Besançon <gilles.besancon@gmail.com>
2017-07-10 14:26:58 +0100

    Allow to parameterize the script.

--- backup/windows/backup.bat
+++ backup/windows/backup.bat
@@ -1,8 +1,13 @@
 REM http://sourceforge.net/projects/backuppc/files/cygwin-rsyncd/3.0.9.0/
 REM http://www.commentcamarche.net/faq/7040-rsync-sous-windows
 
-rsync.exe -auvzr --delete --progress "/cygdrive/g/01-Documents" "/cygdrive/f/Backup"
-rsync.exe -auvzr --delete --progress "/cygdrive/g/02-Images" "/cygdrive/f/Backup"
-echo %DATE% - %TIME% > "F:\Backup\timestamp"
+SET SOURCE_DRIVE=%1
+SET BACKUP_DRIVE=%2
+
+ECHO BACKUP %SOURCE_DRIVE% TO %BACKUP_DRIVE%
+
+rsync.exe -auvzr --delete --progress "/cygdrive/%SOURCE_DRIVE%/01-Documents" "/cygdrive/%BACKUP_DRIVE%/Backup"
+rsync.exe -auvzr --delete --progress "/cygdrive/%SOURCE_DRIVE%/02-Images" "/cygdrive/%BACKUP_DRIVE%/Backup"
+echo %DATE% - %TIME% > "%BACKUP_DRIVE%:\Backup\timestamp.txt"
 
 pause
