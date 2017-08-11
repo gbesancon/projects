64041a3b266a19a4ca8f467c4bdcfe8ba48fc02d
Gilles Besancon <gilles.besancon@gmail.com>
2017-08-11 17:15:52 +0100

    Definition of interface of server and data model.

---
+++ src/main/java/org/benhur/codurance/server/IServer.java
@@ -0,0 +1,15 @@
+package org.benhur.codurance.server;
+
+import java.util.Collection;
+import org.benhur.codurance.model.IMessage;
+
+public interface IServer {
+
+  void postMessage(String user, String message);
+
+  void followUser(String user, String followedUser);
+
+  Collection<IMessage> getTimeline(String user);
+
+  Collection<IMessage> getWall(String user);
+}
