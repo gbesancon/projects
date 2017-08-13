1b9ff0140ab97ce543690a5c3c039a5217035590
Gilles Besancon <gilles.besancon@gmail.com>
2017-08-13 20:06:58 +0100

    Extract CommandLineProcessor as a testable component.

--- src/main/java/org/benhur/codurance/client/Client.java
+++ src/main/java/org/benhur/codurance/client/Client.java
@@ -2,11 +2,6 @@ package org.benhur.codurance.client;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.benhur.codurance.data.IMessage;
-import org.benhur.codurance.data.IUser;
 import org.benhur.codurance.server.IServer;
 
 public class Client {
@@ -21,10 +16,11 @@ public class Client {
     BufferedReader inputBufferReader = new BufferedReader(new InputStreamReader(System.in));
 
     try {
+      CommandLineProcessor commandLineProcessor = new CommandLineProcessor();
       while (true) {
         System.out.print("> ");
         String commandLine = inputBufferReader.readLine();
-        processCommandLine(commandLine.toLowerCase());
+        commandLineProcessor.processCommandLine(commandLine, server);
       }
     } catch (Exception e) {
       System.err.println(e.getMessage());
@@ -34,50 +30,5 @@ public class Client {
 
   private void printInstructions() {
     System.out.println("Enter command, or 'exit' to quit");
-  }
-
-  private void processCommandLine(String commandLine) throws Exception {
-    String exitPattern = "^exit$";
-    String timelinePattern = "^(\\w+)$";
-    String postMessagePattern = "^(\\w+) -> (.+)$";
-    String wallPattern = "^(\\w+) wall$";
-    String followPattern = "^(\\w+) follows (\\w+)$";
-    if (commandLine.matches(exitPattern)) {
-      throw new Exception("Exit");
-    } else if (commandLine.matches(timelinePattern)) {
-      Pattern pattern = Pattern.compile(timelinePattern);
-      Matcher matcher = pattern.matcher(commandLine);
-      if (matcher.find()) {
-        String userName = matcher.group(1);
-        List<IMessage> timeline = server.getTimeline(userName);
-        System.out.println(timeline);
-      }
-    } else if (commandLine.matches(postMessagePattern)) {
-      Pattern pattern = Pattern.compile(postMessagePattern);
-      Matcher matcher = pattern.matcher(commandLine);
-      if (matcher.find()) {
-        String senderName = matcher.group(1);
-        String message = matcher.group(2);
-        IMessage myMessage = server.postMessage(senderName, message);
-        System.out.println(myMessage);
-      }
-    } else if (commandLine.matches(wallPattern)) {
-      Pattern pattern = Pattern.compile(wallPattern);
-      Matcher matcher = pattern.matcher(commandLine);
-      if (matcher.find()) {
-        String userName = matcher.group(1);
-        List<IMessage> wall = server.getWall(userName);
-        System.out.println(wall);
-      }
-    } else if (commandLine.matches(followPattern)) {
-      Pattern pattern = Pattern.compile(followPattern);
-      Matcher matcher = pattern.matcher(commandLine);
-      if (matcher.find()) {
-        String userName = matcher.group(1);
-        String followeeName = matcher.group(2);
-        IUser user = server.followUser(userName, followeeName);
-        System.out.println(user);
-      }
-    }
   }
 }
