96044a9247f6cd25515a6d05621258d2f9ce99e6
Gilles Besançon <GBesancon@slb.com>
2017-08-13 23:19:39 +0100

    Also test exit path in the CommandLineProcessorTest.

--- src/test/java/org/benhur/codurance/client/CommandLineProcessorTest.java
+++ src/test/java/org/benhur/codurance/client/CommandLineProcessorTest.java
@@ -8,77 +8,115 @@ import org.benhur.codurance.data.IUser;
 import org.benhur.codurance.server.IServer;
 import org.hamcrest.core.IsEqual;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CommandLineProcessorTest {
+  @Rule public ExpectedException exceptionGrabber = ExpectedException.none();
+
   @Test
-    public void testPostMessage() throws Exception {
-      CommandLineProcessor commandLineProcessor = new CommandLineProcessor();
-      commandLineProcessor.processCommandLine(
-          "Alice -> I love the weather today",
-          new IServer() {
-  
-            @Override
-            public IMessage postMessage(String senderName, String message) {
-              assertThat(senderName, IsEqual.equalTo("Alice"));
-              assertThat(message, IsEqual.equalTo("I love the weather today"));
-              return null;
-            }
-  
-            @Override
-            public List<IMessage> getWall(String userName) {
-              Assert.fail();
-              return null;
-            }
-  
-            @Override
-            public List<IMessage> getTimeline(String userName) {
-              Assert.fail();
-              return null;
-            }
-  
-            @Override
-            public IUser followUser(String userName, String followedUserName) {
-              Assert.fail();
-              return null;
-            }
-          });
-    }
+  public void testExit() throws Exception {
+    CommandLineProcessor commandLineProcessor = new CommandLineProcessor();
+    exceptionGrabber.expect(Exception.class);
+    commandLineProcessor.processCommandLine(
+        "exit",
+        new IServer() {
 
-@Test
-    public void testTimeline() throws Exception {
-      CommandLineProcessor commandLineProcessor = new CommandLineProcessor();
-      commandLineProcessor.processCommandLine(
-          "Alice",
-          new IServer() {
-  
-            @Override
-            public IMessage postMessage(String senderName, String message) {
-              Assert.fail();
-              return null;
-            }
-  
-            @Override
-            public List<IMessage> getWall(String userName) {
-              Assert.fail();
-              return null;
-            }
-  
-            @Override
-            public List<IMessage> getTimeline(String userName) {
-              assertThat(userName, IsEqual.equalTo("Alice"));
-              return null;
-            }
-  
-            @Override
-            public IUser followUser(String userName, String followedUserName) {
-              Assert.fail();
-              return null;
-            }
-          });
-    }
+          @Override
+          public IMessage postMessage(String senderName, String message) {
+            Assert.fail();
+            return null;
+          }
 
-@Test
+          @Override
+          public List<IMessage> getWall(String userName) {
+            Assert.fail();
+            return null;
+          }
+
+          @Override
+          public List<IMessage> getTimeline(String userName) {
+            Assert.fail();
+            return null;
+          }
+
+          @Override
+          public IUser followUser(String userName, String followedUserName) {
+            Assert.fail();
+            return null;
+          }
+        });
+  }
+
+  @Test
+  public void testPostMessage() throws Exception {
+    CommandLineProcessor commandLineProcessor = new CommandLineProcessor();
+    commandLineProcessor.processCommandLine(
+        "Alice -> I love the weather today",
+        new IServer() {
+
+          @Override
+          public IMessage postMessage(String senderName, String message) {
+            assertThat(senderName, IsEqual.equalTo("Alice"));
+            assertThat(message, IsEqual.equalTo("I love the weather today"));
+            return null;
+          }
+
+          @Override
+          public List<IMessage> getWall(String userName) {
+            Assert.fail();
+            return null;
+          }
+
+          @Override
+          public List<IMessage> getTimeline(String userName) {
+            Assert.fail();
+            return null;
+          }
+
+          @Override
+          public IUser followUser(String userName, String followedUserName) {
+            Assert.fail();
+            return null;
+          }
+        });
+  }
+
+  @Test
+  public void testTimeline() throws Exception {
+    CommandLineProcessor commandLineProcessor = new CommandLineProcessor();
+    commandLineProcessor.processCommandLine(
+        "Alice",
+        new IServer() {
+
+          @Override
+          public IMessage postMessage(String senderName, String message) {
+            Assert.fail();
+            return null;
+          }
+
+          @Override
+          public List<IMessage> getWall(String userName) {
+            Assert.fail();
+            return null;
+          }
+
+          @Override
+          public List<IMessage> getTimeline(String userName) {
+            assertThat(userName, IsEqual.equalTo("Alice"));
+            return null;
+          }
+
+          @Override
+          public IUser followUser(String userName, String followedUserName) {
+            Assert.fail();
+            return null;
+          }
+        });
+  }
+
+  @Test
   public void testWall() throws Exception {
     CommandLineProcessor commandLineProcessor = new CommandLineProcessor();
     commandLineProcessor.processCommandLine(
@@ -111,7 +149,7 @@ public class CommandLineProcessorTest {
         });
   }
 
-@Test
+  @Test
   public void testFollows() throws Exception {
     CommandLineProcessor commandLineProcessor = new CommandLineProcessor();
     commandLineProcessor.processCommandLine(
