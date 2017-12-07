d4aef4bb9f0383eacca64c7958dd568070678082
Gilles Besançon <gilles.besancon@gmail.com>
2017-12-07 13:28:27 +0000

    Step 1: Code for the checkout system

--- src/test/scala/org/benhur/capgemini/fruitshop/CheckoutTest.scala
+++ src/test/scala/org/benhur/capgemini/fruitshop/CheckoutTest.scala
@@ -5,26 +5,39 @@ import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import org.scalatest.junit.JUnitRunner
 
+/**
+ * Class which test the Checkout mechnism.
+ */
 @RunWith(classOf[JUnitRunner])
 class CheckoutTest extends FlatSpec with Matchers {
+  
+  /**
+   * Verify an apple cost 60p. 
+   */
   "1 apple" should "cost 60p" in {
     val checkout = new Checkout();
-    val cart = Array("Apple")
+    val cart = new Cart(List(Apple))
     val cost = checkout.computeCost(cart)
     cost should be (0.60)
   }
     
+  /**
+   * Verify an orange cost 25p.
+   */
   "1 orange" should "cost 25p" in {
     val checkout = new Checkout();
-    val cart = Array("Orange")
+    val cart = new Cart(List(Orange))
     val cost = checkout.computeCost(cart)
     cost should be (0.25)
   }
     
-  "3 apples and 1 orange" should "cost £2.05" in {
+  /**
+   * Verify 3 apples and 1 orange cost ï¿½2.05.
+   */
+  "3 apples and 1 orange" should "cost Â£2.05" in {
     val checkout = new Checkout();
-    val cart = Array("Apple", "Orange", "Apple", "Apple")
+    val cart = new Cart(List(Apple, Apple, Orange, Apple))
     val cost = checkout.computeCost(cart)
-    cost should be (0.25)
+    cost should be (2.05)
   }
 }
