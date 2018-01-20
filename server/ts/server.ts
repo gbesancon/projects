df1d6399cf4502704e3509a66e06091d68802fb8
Gilles Besancon <gilles.besancon@gmail.com>
2018-01-20 17:25:45 +0000

    Reorganize the repository in 2 sub-projects server/browser

--- server/ts/server.ts
+++ server/ts/server.ts
@@ -4,11 +4,12 @@ import * as express from 'express';
 import * as errorHandler from 'errorhandler';
 
 const app = express();
-app.use(express.static('.', { 'index': false }));
+app.use('/node_modules', express.static(path.join(__dirname, '../../../browser/node_modules')))
+app.use('/js', express.static(path.join(__dirname, '../../../browser/build/js')))
 app.set('port', process.env.PORT || '80');
 app.get('/',
     function(req, res, next) {
-        return res.sendFile(path.join(__dirname, '../../html', 'index.html'));
+        return res.sendFile(path.join(__dirname, '../../../browser/html', 'index.html'));
     }
 );
 // error handling middleware should be loaded after the loading the routes
