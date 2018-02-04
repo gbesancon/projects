4f1722caead1688aaa666814ae5301d51cca0dc2
Gilles Besancon <gilles.besancon@gmail.com>
2018-02-04 09:23:49 +0000

    Convert project to gradle.

---
+++ org.benhur.utility.database.dependencies/src/main/java/org/benhur/utility/database/dependencies/ITable.java
@@ -0,0 +1,18 @@
+// Copyright (C) 2017 GBesancon
+
+package org.benhur.utility.database.dependencies;
+
+import java.util.List;
+
+public interface ITable
+{
+  IDatabase getDatabasse();
+
+  String getId();
+
+  String getName();
+
+  void addColumn(IColumn column);
+
+  List<IColumn> getColumns();
+}
---
+++ org.benhur.utility.database.dependencies/src/main/java/org/benhur/utility/database/dependencies/Table.java
@@ -0,0 +1,52 @@
+// Copyright (C) 2017 GBesancon
+
+package org.benhur.utility.database.dependencies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Table implements ITable
+{
+  protected final IDatabase database;
+  protected final String id;
+  protected final String name;
+  protected final List<IColumn> columns;
+
+  public Table(IDatabase database, String id, String name)
+  {
+    this.database = database;
+    this.id = id;
+    this.name = name;
+    this.columns = new ArrayList<>();
+  }
+
+  @Override
+  public IDatabase getDatabasse()
+  {
+    return database;
+  }
+
+  @Override
+  public String getId()
+  {
+    return id;
+  }
+
+  @Override
+  public String getName()
+  {
+    return name;
+  }
+
+  @Override
+  public void addColumn(IColumn column)
+  {
+    columns.add(column);
+  }
+
+  @Override
+  public List<IColumn> getColumns()
+  {
+    return columns;
+  }
+}
