89ab6fd4f28acfdd40fa1bfb61a186339311f888
Gilles Besançon <gilles.besancon@gmail.com>
2018-06-19 13:33:40 +0100

    Split client / server to access pi informations.

--- browser/ts/piviz.ts
+++ browser/ts/piviz.ts
@@ -1,16 +1,12 @@
 "use strict";
 
-import pi = require("./data/pi");
-
 class PiViz {
-    private pi: pi.Pi;
-
     constructor() {
-        this.pi = new pi.Pi();
+        //
     }
 
     public run() {
-        this.pi.load();
+        //
     }
 }
 
