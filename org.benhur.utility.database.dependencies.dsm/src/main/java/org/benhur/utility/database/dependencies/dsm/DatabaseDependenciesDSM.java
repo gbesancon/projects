3b77f71f85adf7adbcee0f054f8f43ea16c48993
Gilles Besancon <gilles.besancon@gmail.com>
2018-08-01 17:30:40 -0500

    Create a package allowing to run the jar as a command line.

--- org.benhur.utility.database.dependencies.dsm/src/main/java/org/benhur/utility/database/dependencies/dsm/DatabaseDependenciesDSM.java
+++ org.benhur.utility.database.dependencies.dsm/src/main/java/org/benhur/utility/database/dependencies/dsm/DatabaseDependenciesDSM.java
@@ -1,15 +1,14 @@
 // Copyright (C) 2017 GBesancon
 
-package org.benhur.utility.database.dependencies.dgml;
+package org.benhur.utility.database.dependencies.dsm;
 
 import java.io.IOException;
 import javax.xml.bind.PropertyException;
 import org.benhur.utility.database.dependencies.IDatabase;
 import org.benhur.utility.database.dependencies.builder.DatabaseBuilder;
 import org.benhur.utility.database.dependencies.configuration.Configuration;
-import org.benhur.utility.dgml.DGMLException;
 
-public class MainDatabaseDependenciesDGML {
+public class DatabaseDependenciesDSM {
   public static void main(String[] args) {
     if (args.length == 1) {
       try {
@@ -23,14 +22,11 @@ public class MainDatabaseDependenciesDGML {
                 configuration.getPassword(),
                 configuration.getDatabaseName());
 
-        DatabaseDGMLFileBuilder databaseDGMLFileBuilder = new DatabaseDGMLFileBuilder();
-        databaseDGMLFileBuilder.createDGMLFile(
-            database, configuration, database.getName() + ".dgml");
+        DatabaseDSMFileBuilder databaseDSMFileBuilder = new DatabaseDSMFileBuilder();
+        databaseDSMFileBuilder.createDSMFile(database, configuration, database.getName() + ".dsm");
       } catch (PropertyException e) {
         e.printStackTrace();
       } catch (IOException e) {
-        e.printStackTrace();
-      } catch (DGMLException e) {
         e.printStackTrace();
       }
     } else {
