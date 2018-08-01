97e1f482cecc1daa3904ce132c2fe635c5af892e
Gilles Besançon <gilles.besancon@gmail.com>
2018-08-01 16:24:27 -0500

    Use new shared build-gradle-scripts.

--- org.benhur.utility.dgml/src/main/java/org/benhur/utility/dgml/Category.java
+++ org.benhur.utility.dgml/src/main/java/org/benhur/utility/dgml/Category.java
@@ -2,8 +2,7 @@
 
 package org.benhur.utility.dgml;
 
-public class Category
-{
+public class Category {
   protected final String id;
   protected final String label;
   protected final String description;
@@ -13,10 +12,15 @@ public class Category
   protected final boolean isContainment;
   protected final String outgoingActionLabel;
 
-  public Category(String id, String label, String description, boolean canBeDataDriven,
-                  boolean canLinkedNodesBeDataDriven, String incomingActionLabel, boolean isContainment,
-                  String outgoingActionLabel)
-  {
+  public Category(
+      String id,
+      String label,
+      String description,
+      boolean canBeDataDriven,
+      boolean canLinkedNodesBeDataDriven,
+      String incomingActionLabel,
+      boolean isContainment,
+      String outgoingActionLabel) {
     this.id = id;
     this.label = label;
     this.description = description;
--- org.benhur.utility.dgml/src/main/java/org/benhur/utility/dgml/Link.java
+++ org.benhur.utility.dgml/src/main/java/org/benhur/utility/dgml/Link.java
@@ -2,14 +2,12 @@
 
 package org.benhur.utility.dgml;
 
-public class Link
-{
+public class Link {
   public final Node source;
   public final Node target;
   public final Category category;
 
-  public Link(Node source, Node target, Category category)
-  {
+  public Link(Node source, Node target, Category category) {
     this.source = source;
     this.target = target;
     this.category = category;
