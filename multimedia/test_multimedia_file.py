81f430f73899bd50a9721cc2abe81d4148ef4d79
Gilles Besançon <gilles.besancon@gmail.com>
2018-10-13 19:44:08 -0500

    Refactoring functions.

--- multimedia/test_multimedia_file.py
+++ multimedia/test_multimedia_file.py
@@ -3,7 +3,7 @@ import multimedia_file
 
 class Test_CheckMultimediaFile(unittest.TestCase):
     def test_check_files_in_folders(self):
-        valid = multimedia_file.main(["-d", "H:\\02-Photos\\01-Personal"])
+        valid = multimedia_file.main(["-d", "H:\\02-Photos\\01-Personal\\2018"])
         self.assertFalse(valid)
 
 if __name__ == '__main__':
