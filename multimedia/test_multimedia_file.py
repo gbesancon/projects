7e3ad2e56b1baf0de146a3990588b6f3fa47afb6
Gilles Besançon <gilles.besancon@gmail.com>
2018-10-13 19:10:16 -0500

    Put together check and process multimedia files script.

---
+++ multimedia/test_multimedia_file.py
@@ -0,0 +1,10 @@
+import unittest
+import multimedia_file
+
+class Test_CheckMultimediaFile(unittest.TestCase):
+    def test_check_files_in_folders(self):
+        valid = multimedia_file.main(["-d", "H:\\02-Photos\\01-Personal"])
+        self.assertFalse(valid)
+
+if __name__ == '__main__':
+    unittest.main()
