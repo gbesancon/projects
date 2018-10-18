83dc0bb0139f8cf77563a9c8adfa499946070703
Gilles Besançon <gilles.besancon@gmail.com>
2018-10-18 00:20:44 -0500

    Split multimedia_file into multiple files.

---
+++ multimedia/video_file.py
@@ -0,0 +1,14 @@
+
+VIDEO_PREFIX = "v"
+VIDEO_EXTENSION_PREFIX = {    
+    ".avi" : VIDEO_PREFIX,
+    ".mp4" : VIDEO_PREFIX,
+    ".mov" : VIDEO_PREFIX,
+    ".mpg" : VIDEO_PREFIX,
+    ".mpeg": VIDEO_PREFIX,
+    ".mts" : VIDEO_PREFIX,
+    ".wmv" : VIDEO_PREFIX,
+    ".3gp" : VIDEO_PREFIX,
+    ".flv" : VIDEO_PREFIX,
+    ".ogv" : VIDEO_PREFIX,
+}
