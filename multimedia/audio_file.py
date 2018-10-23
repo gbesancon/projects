354250c3749fd4686768ea9a024e6bccfa4fa5b2
Gilles Besançon <gilles.besancon@gmail.com>
2018-10-23 06:21:24 -0500

    move specific methods to files.

--- multimedia/audio_file.py
+++ multimedia/audio_file.py
@@ -1,4 +1,7 @@
 import file
+import file_messages
+import multimedia_file
+import os
 
 AUDIO_PREFIX = "a"
 AUDIO_EXTENSION_PREFIX = {    
@@ -13,3 +16,61 @@ def is_audio_file(file_path):
 
 def set_audio_file_date(file_path, file_date):
     file.set_file_date(file_path, file_date)
+    
+def check_audio_file_date(file_path):
+    (_, file_date) = multimedia_file.get_multimedia_file_date(file_path, None)
+    return multimedia_file.check_multimedia_file_date(file_path, file_date)
+    
+def set_audio_file_dates(folder_path, audio_file_names, process, verbose):
+    files_processed = False
+    files_process_comments = {}
+
+    for audio_file_name in audio_file_names:
+        audio_file_path = os.path.join(folder_path, audio_file_name)
+        (file_date_valid, file_date) = multimedia_file.get_multimedia_file_date(audio_file_path, None)
+        (folder_date_valid, folder_date) = multimedia_file.has_valid_dated_folder_name(audio_file_path)
+        if file_date_valid:
+            if folder_date_valid:
+                if file_date.year == folder_date.year and file_date.month == folder_date.month and file_date.day == folder_date.day:
+                    if process:
+                        set_audio_file_date(audio_file_path, file_date)
+                    if not process or verbose:
+                        file_messages.add_file_message(files_process_comments, audio_file_path, "Set File date (" + str(file_date) + ")")
+                else:
+                    file_messages.add_file_message(files_process_comments, audio_file_path, "File date (" + str(file_date) + ") not matching dated folder (" + folder_path + ")")
+            else:
+                pass
+        else:
+            if folder_date_valid:
+                if process:
+                    set_audio_file_date(audio_file_path, folder_date)
+                if not process or verbose:
+                    file_messages.add_file_message(files_process_comments, audio_file_path, "Set File date (" + str(folder_date) + ")")
+            else:
+                file_messages.add_file_message(files_process_comments, audio_file_path, "No date identified for file")
+            
+    files_processed = process
+    return (files_processed, files_process_comments)
+
+def rename_audio_files(folder_path, audio_file_names, process, verbose):
+    files_processed = False
+    files_process_comments = {}
+    
+    files_processed = process
+    return (files_processed, files_process_comments)
+
+def process_audio_files_in_folder(folder_path, audio_file_names, process, verbose):
+    files_processed = False
+    files_process_comments = {}
+
+    # Change file dates
+    (audio_file_dates_processed, audio_file_dates_process_comments) = set_audio_file_dates(folder_path, audio_file_names, process, verbose)
+    files_processed &= audio_file_dates_processed
+    file_messages.add_file_messages(files_process_comments, audio_file_dates_process_comments)
+    # Rename files
+    (rename_audio_files_processed, rename_audio_files_process_comments) = rename_audio_files(folder_path, audio_file_names, process, verbose)
+    files_processed &= rename_audio_files_processed
+    file_messages.add_file_messages(files_process_comments, rename_audio_files_process_comments)
+    
+    files_processed = process
+    return (files_processed, files_process_comments)
---
+++ multimedia/multimedia_file.py
@@ -0,0 +1,123 @@
+import re
+import datetime
+import file
+import os
+
+def is_year_folder_name(folder_name):
+    valid = True
+    match = re.match(r"^\d\d\d\d$", folder_name)
+    if match:
+        valid = True
+    else:
+        valid = False
+    return valid
+
+def is_valid_dated_folder_name(folder_name):
+    valid = True
+    date = None
+    match = re.match(r"^(\d\d\d\d-\d\d-\d\d)[a-z]?\s-\s.*$", folder_name)
+    if match:
+        valid = True
+        date = datetime.datetime.strptime(match.group(1), r'%Y-%m-%d')
+    else:
+        valid = False
+    return (valid, date)
+
+def has_valid_dated_folder_name(file_path):
+    valid = False
+    folder_name = ""
+    while not valid and not is_year_folder_name(folder_name):
+        folder_path = file.get_folder_path(file_path)
+        folder_name = file.get_folder_name(file_path)
+        (valid, date) = is_valid_dated_folder_name(folder_name)
+        file_path = folder_path
+    return (valid, date)
+
+def get_date_from_folder_name(file_path):
+    (_, date) = has_valid_dated_folder_name(file_path)
+    return date
+
+def is_valid_period_dated_folder_name(folder_name):
+    valid = True
+    beginning_date = None
+    end_date = None
+    match = re.match(r"^(\d\d\d\d-\d\d-\d\d)[a-z]?_(\d\d\d\d-\d\d-\d\d)[a-z]?\s-\s.*$", folder_name)
+    if match:
+        valid = True
+        beginning_date = datetime.datetime.strptime(match.group(1), r'%Y-%m-%d')
+        end_date = datetime.datetime.strptime(match.group(2), r'%Y-%m-%d') + datetime.timedelta(days=1) - datetime.timedelta(microseconds=1)
+    else:
+        valid = False
+    return (valid, beginning_date, end_date)
+
+def has_valid_period_dated_folder_name(file_path):
+    valid = False
+    folder_name = ""
+    while not valid and not is_year_folder_name(folder_name):
+        folder_path = file.get_folder_path(file_path)
+        folder_name = file.get_folder_name(file_path)
+        (valid, date) = is_valid_dated_folder_name(folder_name)
+        if valid:
+            beginning_date = date
+            end_date = date + datetime.timedelta(days=1) - datetime.timedelta(microseconds=1)
+        else:
+            (valid, beginning_date, end_date) = is_valid_period_dated_folder_name(folder_name)
+        file_path = folder_path
+    return (valid, beginning_date, end_date)
+    
+def get_multimedia_file_date(file_path, get_file_date):    
+    file_date_valid = False
+    file_date = None
+    if get_file_date:
+        (file_date_valid, file_date) = get_file_date(file_path)
+    if not file_date_valid:
+        (folder_date_valid, folder_date) = has_valid_dated_folder_name(file_path)
+        if folder_date_valid:
+            file_date_valid = True
+            file_date = folder_date
+        else:
+            file_date = None
+    return (file_date_valid, file_date)
+
+def validate_file_location(file_date, file_path):
+    file_location_valid = False
+    (folder_date_valid, folder_date) = has_valid_dated_folder_name(file_path)
+    if folder_date_valid:
+        if file_date:
+            if file_date.year == folder_date.year and file_date.month == folder_date.month and file_date.day == folder_date.day:
+                file_location_valid = True
+    else:
+        (folder_period_date_valid, folder_period_beginning_date, folder_period_end_date) = has_valid_period_dated_folder_name(file_path)
+        if folder_period_date_valid:
+            if file_date:
+                if file_date >= folder_period_beginning_date and file_date <= folder_period_end_date:
+                    file_location_valid = True
+    return file_location_valid
+    
+def check_multimedia_file_date(file_path, file_date):
+    file_date_valid = True
+    file_error_message = None
+    if file_date_valid:
+        (file_date_valid, file_date, file_error_message) = file.check_file_dates(file_path, "File date", lambda f : file_date, "Folder date", get_date_from_folder_name, 24*60*60)
+    if file_date_valid:
+        (file_date_valid, file_date, file_error_message) = file.check_file_dates(file_path, "File date", lambda f : file_date, "Creation date", file.get_creation_date)
+    if file_date_valid:
+        (file_date_valid, file_date, file_error_message) = file.check_file_dates(file_path, "File date", lambda f : file_date, "Modification date", file.get_modification_date)
+    return (file_date_valid, file_date, file_error_message)
+    
+def has_valid_extension(file_path, file_extensions):
+    file_extension = file.get_file_extension(file_path)
+    return file_extension in file_extensions
+
+def is_valid_file_name(file_path, file_extensions):
+    valid = True
+    file_name = file.get_file_name(file_path)
+    file_name_no_extension = os.path.splitext(file_name)[0]
+    file_extension = file.get_file_extension(file_path)
+    prefix = file_extensions[file_extension]
+    match = re.match(r"^" + prefix + r"\d\d\d\d\d$", file_name_no_extension)
+    if match:
+        valid = True
+    else:
+        valid = False
+    return valid
