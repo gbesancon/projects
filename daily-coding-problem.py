281c466f4ca78277c49e2a3016a4e12b08ac08d0
Gilles Besancon <GBesancon@slb.com>
2019-01-03 11:20:14 -0600

    Add new problems.

--- daily-coding-problem.py
+++ daily-coding-problem.py
@@ -2,15 +2,15 @@ import sys
 import os
 
 def main(argv):
-    for i in range(1,int(argv[0])):
-        problem_folderpath = str(i)
+    for i in range(1,int(argv[0])+1):
+        problem_folderpath = os.path.join('problems',str(i))
         if not os.path.exists(problem_folderpath):
             os.makedirs(problem_folderpath)
-        readme_filepath = os.path.join(str(i),"README.txt")
+        readme_filepath = os.path.join(problem_folderpath,"README.txt")
         if not os.path.exists(readme_filepath):
             with open(readme_filepath, "w") as readme_file: 
                 readme_file.write("Daily Coding Problem: Problem #" + str(i) + "\n")
-        problem_filepath = os.path.join(str(i),"problem_" + str(i) + ".py")
+        problem_filepath = os.path.join(problem_folderpath,"problem_" + str(i) + ".py")
         if not os.path.exists(problem_filepath):
             with open(problem_filepath, "w") as problem_file: 
                 problem_file.write("import sys\n\n")
