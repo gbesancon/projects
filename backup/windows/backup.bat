59c9e39290d740ece8ff06367f678a5fad4c67cb
Gilles Besançon <gilles.besancon@gmail.com>
2019-05-25 10:40:28 -0500

    Rename Image -> Multimedia

--- backup/windows/backup.bat
+++ backup/windows/backup.bat
@@ -7,7 +7,7 @@ SET BACKUP_DRIVE=%2
 ECHO BACKUP %SOURCE_DRIVE% TO %BACKUP_DRIVE%
 
 rsync.exe -auvzr --delete --progress "/cygdrive/%SOURCE_DRIVE%/01-Documents" "/cygdrive/%BACKUP_DRIVE%/Backup"
-rsync.exe -auvzr --delete --progress "/cygdrive/%SOURCE_DRIVE%/02-Images" "/cygdrive/%BACKUP_DRIVE%/Backup"
+rsync.exe -auvzr --delete --progress "/cygdrive/%SOURCE_DRIVE%/02-Multimedia" "/cygdrive/%BACKUP_DRIVE%/Backup"
 echo %DATE% - %TIME% > "%BACKUP_DRIVE%:\Backup\timestamp.txt"
 
 pause
