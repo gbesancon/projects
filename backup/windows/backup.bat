79fc4d1e5fbd828501e86caa5156cc7feb4034f5
Gilles Besancon <gilles.besancon@gmail.com>
2019-12-08 19:21:56 -0600

    Create scripts for dryrun execution.

--- backup/windows/backup.bat
+++ backup/windows/backup.bat
@@ -6,8 +6,7 @@ SET BACKUP_DRIVE=%2
 
 ECHO BACKUP %SOURCE_DRIVE% TO %BACKUP_DRIVE%
 
-rsync.exe -auvzr --delete --progress "/cygdrive/%SOURCE_DRIVE%/01-Documents" "/cygdrive/%BACKUP_DRIVE%/Backup"
-rsync.exe -auvzr --delete --progress "/cygdrive/%SOURCE_DRIVE%/02-Multimedia" "/cygdrive/%BACKUP_DRIVE%/Backup"
+rsync.exe -auvzr --delete --progress "/cygdrive/%SOURCE_DRIVE%/" "/cygdrive/%BACKUP_DRIVE%"
 echo %DATE% - %TIME% > "%BACKUP_DRIVE%:\Backup\timestamp.txt"
 
 pause
