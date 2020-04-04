0d5282e5f780734a678d0061436cd57c655524d1
Gilles Besancon <gilles.besancon@gmail.com>
2020-04-04 17:18:57 -0500

    Easy setup

--- repository/00-github-setup.sh
+++ repository/00-github-setup.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
+set -e
 
-source $(dirname $(realpath ${BASH_SOURCE[0]}))/../linux/utils-github.sh
-source $(dirname $(realpath ${BASH_SOURCE[0]}))/../linux/utils-git.sh
-
-REPOSITORY_NAME=setup
-git_clone_repository $(get_github_repository_url gbesancon $REPOSITORY_NAME) $REPOSITORY_NAME
-exit_on_error $?
+git clone https://github.com/gbesancon/setup
