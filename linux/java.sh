cbede8343eb4e39b150dbd076742e2bc94b0c8b8
Gilles Besançon <gilles.besancon@gmail.com>
2020-04-05 14:09:31 -0500

    Update installer scripts

--- linux/java.sh
+++ linux/java.sh
@@ -4,6 +4,24 @@
 
 source $(dirname $(realpath ${BASH_SOURCE[0]}))/../utils/apt.sh
 
+install_jdk()
+{
+  JAVA_VERSION=$1
+  result=0
+  echo Installing java version $JAVA_VERSION
+  add_ppa_package_repository linuxuprising/java
+  add_ppa_package_repository_result=$?
+  if [ $add_ppa_package_repository_result -eq 0 ]
+  then
+    update_packages oracle-java${JAVA_VERSION}-installer oracle-java${JAVA_VERSION}-set-default
+    update_packages_result=$?
+    result=$update_packages_result
+  else
+    result=$add_ppa_package_repository_result
+  fi
+  return $result
+}
+
 install_or_update_jdk()
 {
   JAVA_VERSION=$1
@@ -12,10 +30,20 @@ install_or_update_jdk()
   check_command_result=$?
   if [ $check_command_result -eq 0 ]
   then
-    update_package oracle-java${JAVA_VERSION}-installer
+    CURRENT_JAVA_VERSION=`java -version 2>&1 | grep -o -e "version \"[[:digit:]]\+" | grep -o -e "[[:digit:]]\+"`
+    if [ $CURRENT_JAVA_VERSION == $JAVA_VERSION ]
+    then
+      echo java version $JAVA_VERSION available
+    else
+      echo java version $CURRENT_JAVA_VERSION available
+      install_jdk $JAVA_VERSION
+      install_jdk_result=$?
+      result=$install_jdk_result
+    fi
   else
-    add_ppa_package_repository linuxuprising/java
-    update_package oracle-java${JAVA_VERSION}-installer
+    install_jdk $JAVA_VERSION
+    install_jdk_result=$?
+    result=$install_jdk_result
   fi
   return $result
 }
