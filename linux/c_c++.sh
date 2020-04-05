d99eb964dbfb387570526f6c144f6b9c10b3652b
Gilles Besancon <gilles.besancon@gmail.com>
2020-04-05 14:09:31 -0500

    Update installer scripts

--- linux/c_c++.sh
+++ linux/c_c++.sh
@@ -4,10 +4,7 @@
 
 source $(dirname $(realpath ${BASH_SOURCE[0]}))/../utils/apt.sh
 
-install_command_from_package apt-get apt-utils
-exit_on_error $?
-
-for command in apt bash curl dialog git wget; do
+for command in cc g++ gcc make cmake; do
   install_command_from_package $command $command
   exit_on_error $?
 done
--- utils/apt.sh
+++ utils/apt.sh
@@ -7,17 +7,49 @@ add_ppa_package_repository()
   PPA=$1
   result=0
   echo Adding apt-repository $PPA
-  execute_command sudo add-apt-repository ppa:$PPA
+  execute_command sudo add-apt-repository -y ppa:$PPA
   add_apt_repository_result=$?
   if [ $add_apt_repository_result -eq 0 ]
   then
     echo apt-repository $PPA added
-    update_package
+    refresh_packages
     result=$?
   else
     echo Failed to add apt-repository $PPA
     result=$add_apt_repository_result
   fi
+  return $result
+}
+
+refresh_packages()
+{
+  result=0
+  echo Refreshing packages
+  execute_command sudo apt-get update
+  update_result=$?
+  if [ $update_result -eq 0 ]
+  then
+    echo Packages refreshed 
+  else
+    echo Failed to refresh packages
+  fi
+  result=$update_result
+  return $result
+}
+
+install_packages()
+{
+  PACKAGES=$@
+  result=0
+  for PACKAGE in $PACKAGES
+  do
+    install_package $PACKAGE
+    install_package_result=$?
+    if [ $install_package_result -ne 0 ]
+    then
+      result=$install_package_result
+    fi
+  done
   return $result
 }
 
@@ -38,21 +70,20 @@ install_package()
   return $result
 }
 
-update_package()
+update_packages()
 {
-  PACKAGE=$1
+  PACKAGES=$@
   result=0
-  echo Updating package $PACKAGE
-  execute_command sudo apt-get update
-  update_result=$?
-  if [ $update_result -eq 0 ]
+  refresh_packages
+  refresh_packages_result=$?
+  if [ $refresh_packages_result -eq 0 ]
   then
-    install_package $PACKAGE
-    update_package_result=$?
-    result=$update_package_result
+    echo Updating packages $PACKAGES
+    install_packages $PACKAGES
+    update_packages_result=$?
+    result=$update_packages_result
   else
-    echo Failed to update packages
-    result=$update_result
+    result=$refresh_packages_result
   fi
   return $result
 }
