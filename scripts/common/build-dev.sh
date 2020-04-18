af0d3eda9a8ff63e386c084b244e1888fab95c56
Gilles Besançon <gilles.besancon@gmail.com>
2020-04-17 21:35:16 -0500

    Refactoring build (#2)
    * Begin to Reorganise folder and Makefile.
    
    * split Makefile into single and composite, create technology specific Makefiles.
    
    * use jakarta xml bind for javax.xml.bind
    
    * Build databasse and visual studio project dependencies projects
    
    * Add azure pipeline configuration.

--- scripts/common/build-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-export VERSION=$1
-
-bash scripts/build.sh $VERSION
--- scripts/common/docker-publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-export VERSION=$1
-
-source ../scripts/CONFIGURATION
-
-docker push $DOCKER_IMAGE_PREFIX/$PROJECT_NAME:$VERSION
