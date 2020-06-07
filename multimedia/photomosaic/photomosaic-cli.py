2f0a80bcaaf1e2b2daa2cd171097ada75ff98765
Gilles Besancon <gilles.besancon@gmail.com>
2020-06-07 15:26:03 -0500

    Rename photomosaic script to -cli

--- multimedia/photomosaic/photomosaic-cli.py
+++ multimedia/photomosaic/photomosaic-cli.py
@@ -14,9 +14,9 @@ class PhotoMosaic:
         parser.add_argument('--image_destination', required=False)
         parser.add_argument('--display', required=False, default=False, action='store_true')
         parser.add_argument('--use-color-tile', required=False, default=False, action='store_true')
-        parser.add_argument('--color-tile-folderpath', required=False, default='color_tile/')
+        parser.add_argument('--color-tile-folderpath', required=False, default='/tmp/color_tile/')
         parser.add_argument('--use-image-tile', required=False, default=False, action='store_true')
-        parser.add_argument('--image-tile-folderpath', required=False)
+        parser.add_argument('--image-tile-folderpath', required=False, default='/tmp/image_tile')
         parsed_args = parser.parse_args(args)
         return parsed_args
 
@@ -35,7 +35,7 @@ class PhotoMosaic:
         return pool
 
     def create_mosaic(self, image, pool, n_width, n_length) -> any:
-        mosaic = photomosaic.bassic_mosaic(image, pool, (n_width, n_length))
+        mosaic = photomosaic.basic_mosaic(image, pool, (n_width, n_length))
         return mosaic
 
     def save_mosaic(self, mosaic: any, filepath: str) -> any:
@@ -47,15 +47,16 @@ class PhotoMosaic:
     def main(self, args: dict):
         parsed_args = self.parse_args(args)
         image = self.load_image(parsed_args.image_source)
+        pool = None
         if parsed_args.use_color_tile:
-            self.create_color_pool(parsed_args.color_tile_filepath)
-            pool = self.create_pool()
+            self.create_color_pool(parsed_args.color_tile_folderpath)
+            pool = self.create_pool(parsed_args.color_tile_folderpath)
         elif parsed_args.use_image_tile:
             pass
         else:
             print("unknown tile type")
             return
-        mosaic = self.create_mosaic(image, pool, self.width, self.length)
+        mosaic = self.create_mosaic(image, pool, parsed_args.width, parsed_args.length)
         if parsed_args.image_destination:
             self.save_mosaic(mosaic, parsed_args.image_destination)
         if parsed_args.display:
@@ -63,5 +64,5 @@ class PhotoMosaic:
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    args += ["--image-source", "/home/gbesancon/Downloads/2019-08-16b - Gallery/EleftheriaGilles-1.jpg", "--width", "30", "--length", "30", "--display", "--use-color-tile"]
+    args += ["--image-source", "/home/gbesancon/Downloads/2019-08-16b - Gallery/EleftheriaGilles-1.jpg", "--width", "30", "--length", "30", "--use-color-tile", "--image-destination", "EleftheriaGilles-1.jpg", "--display"]
     PhotoMosaic().main(args)
