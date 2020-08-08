c4f97da2157ba6a639dbe7d1f2431cb30def9daa
Gilles Besançon <gilles.besancon@gmail.com>
2020-08-08 11:34:10 -0500

    Add more material for learning greek

--- 42/language/greek.cs
+++ 42/language/greek.cs
@@ -1,1 +1,16 @@
+https://www.hau.gr/?i=learning.el.podcasts-in-greek
+Βιβλιο Α δημοτικου 
+http://ebooks.edu.gr/ebooks/v/html/8547/1993/Glossa_A-Dimotikou_html-empl/indexb_00.html
+
+https://www.youtube.com/channel/UCUDJWYidxNHanwrRtBfcwpw
+
+https://dschool.edu.gr/
+
+https://www.foundalis.com/lan/grstress.htm
+
+https://www.greekgrammar.eu/
+Video for Μάθημα 1: Συστάσεις (1)
+https://www.hau.gr/?i=learning.el.mg-video_1
+
+
 https://www.lawlessgreek.com/
